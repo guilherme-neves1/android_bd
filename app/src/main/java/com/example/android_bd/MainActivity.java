427183e30db12d25391564291651e4322aa68835
Guilherme-Neves1 <guilherme.neves20@hotmail.com>
2024-04-19 21:54:58 -0300

    add buttons and edittext on MainActivity.java

--- app/src/main/java/com/example/android_bd/MainActivity.java
+++ app/src/main/java/com/example/android_bd/MainActivity.java
@@ -3,12 +3,23 @@ package com.example.android_bd;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements OnClickListener {
+
+    Button btnSave, btnConsult, btnAlter, btnExclude;
+
+    EditText txtCode, txtName, txtEmail;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        txtCode = (EditText) findViewById(R.id.txtCode);
+        txtName = (EditText) findViewById(R.id.txtName);
+        txtEmail = (EditText) findViewById(R.id.txtEmail);
     }
 }
---
+++ app/src/main/java/com/example/android_bd/createDataBase.java
@@ -0,0 +1,4 @@
+package com.example.android_bd;
+
+public class createDataBase {
+}
